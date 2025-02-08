96f2bb4544eca7d7608a43d781fe162d1a9e8e87
rasme54 <rsmektala54@gmail.com>
2025-02-09 00:07:27 +0100

    continuation of  bulding the automation for test case 1.

--- cypress/integration/E2E-TestCases.js
+++ cypress/integration/E2E-TestCases.js
@@ -10,8 +10,13 @@ describe("E2E - Test Cases", () => {
         cy.get("div[class='signup-form']").should("be.visible")
 
         // entering sign-up data
-        cy.get("input[placeholder='Name']").type("testUserName")
-        cy.get("input[placeholder='Email Address']").eq(1).type("testusername@MediaList.pl")
+        cy.get("input[placeholder='Name']").as("nameInput")
+        cy.get("input[placeholder='Email Address']").eq(1).as("emailInput")
+
+        cy.get("@nameInput").type("testUserName")
+        cy.get("@nameInput").invoke("val").as("valueNameInput")
+        cy.get("@emailInput").type("testusername@MediaList.pl")
+        cy.get("@emailInput").invoke("val").as("valueEmailInput")
         // confirming data
         cy.get("button[type='submit']").contains("Signup").click()
 
@@ -20,9 +25,33 @@ describe("E2E - Test Cases", () => {
         cy.get("b").contains("Enter Account Information").should("be.visible")
         
         // entering data
-        //radio
+        // radio
         cy.get("#id_gender1").check()
         cy.get("#id_gender1").should("be.checked")
+        // name
+        cy.get("@valueNameInput").then((checkedNameInput) => {
+            cy.get("#name").should("have.value", checkedNameInput)
+        })
+        // email
+        cy.get("@valueEmailInput").then((checkedEmailInput) => {
+            cy.get("#email").should("have.value", checkedEmailInput)
+        })
+
+        // password
+        cy.get("#password").type("PASSword123!")
+
+        // date of birth - selecting option in selector component in three different ways
+        // day - by text
+        cy.get("select[id='days']").select("14")
+        // month - by index
+        cy.get("select[id='months']").select(4)
+        // year - by value attribut
+        cy.get("select[id='years']").select("2019")
+
+        // newsletter
+        cy.get("#newsletter").check()
+        // special offers
+        cy.get("#optin").check()
 
     })
 })
