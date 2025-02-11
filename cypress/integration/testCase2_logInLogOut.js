35da769acec3503931fbd2cf3b0f637ae9519e26
rasme54 <rsmektala54@gmail.com>
2025-02-11 22:33:31 +0100

    second test case

--- cypress/integration/testCase2_logInLogOut.js
+++ cypress/integration/testCase2_logInLogOut.js
@@ -3,6 +3,11 @@
 
 describe("TC2 - LogInLogOut", () => {
     it("Log In", () => {
+        // navigate and check visability of the website
         cy.visit("/")
+        cy.get("body").should("be.visible")
+
+        cy.contains(" Signup / Login"). click()
+        cy.contains("Login to your account").should("be.visible")
     })
 })
