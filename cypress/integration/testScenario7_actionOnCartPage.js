476656c0826c72da5b7375f6fb48d53abdf263a4
rasme54 <rsmektala54@gmail.com>
2025-03-02 21:03:51 +0100

    adding new test cases

--- cypress/integration/testScenario7_actionOnCartPage.js
+++ cypress/integration/testScenario7_actionOnCartPage.js
@@ -12,16 +12,31 @@ describe("TS7 - addingProductToCart", () => {
   const utils = new Utils();
   const productPage = new ProductsPage();
 
-  it("Add Two Products to Cart", () => {
+  const productNumberOne = 0;
+  const productNumberTwo = 1;
+
+  it("12. Add Two Products to Cart", () => {
     homePage.selectProductPage();
-    productPage.hoverAndAddToCart(0);
+    productPage.hoverAndAddToCart(productNumberOne);
     utils.isStringContains("p[class='text-center']", "Your product has been added to cart.");
     actionOnPage.clickButton('button[class="btn btn-success close-modal btn-block"]');
-    productPage.hoverAndAddToCart(1);
+    productPage.hoverAndAddToCart(productNumberTwo);
     utils.isStringContains("p[class='text-center']", "Your product has been added to cart.");
     actionOnPage.clickButton('button[class="btn btn-success close-modal btn-block"]');
     homePage.selectCartPage();
     cartPage.isNumberOfElementsEqualTo("tbody > tr", 2);
     cartPage.checkDetailsOfProduct("Rs. 400", "Rs. 500");
+  });
+  it("17. Remove Products from Cart", () => {
+    homePage.selectProductPage();
+    cy.addToCart(productNumberOne);
+    cy.addToCart(productNumberTwo);
+    homePage.selectCartPage();
+    cartPage.isNumberOfElementsEqualTo("tbody > tr", 2);
+    utils.isPageUrlCorrect("/view_cart");
+    cy.get("a[class='cart_quantity_delete']").eq(1).as("secondProduct");
+    actionOnPage.clickButton("@secondProduct");
+    // 7. Click 'X' button corresponding to particular product
+    // 8. Verify that product is removed from the cart
   });
 });
--- cypress/integration/testScenario9_registrationLoginDuringThePurchasing.js
+++ cypress/integration/testScenario9_registrationLoginDuringThePurchasing.js
@@ -89,20 +89,21 @@ describe("TS9 - placeOrder", () => {
   });
 
   it.only("16. Place Order: Login before Checkout", function () {
+    const productNumber = 9;
     homePage.selectLoginPage();
     loginPage.logIn(this.loginData);
     utils.isUserLogged(this.loginData);
-
-    // 7. Add products to cart
-    // 8. Click 'Cart' button
-    // 9. Verify that cart page is displayed
-    // 10. Click Proceed To Checkout
-    // 11. Verify Address Details and Review Your Order
-    // 12. Enter description in comment text area and click 'Place Order'
-    // 13. Enter payment details: Name on Card, Card Number, CVC, Expiration date
-    // 14. Click 'Pay and Confirm Order' button
-    // 15. Verify success message 'Your order has been placed successfully!'
-    // 16. Click 'Delete Account' button
-    // 17. Verify 'ACCOUNT DELETED!' and click 'Continue' button
+    homePage.selectProductPage();
+    cy.addToCart(productNumber);
+    homePage.selectCartPage();
+    cartPage.proceedToCheckout();
+    checkoutPage.CheckProductDetails(this.newUser);
+    actionOnPage.typeInputValue("div[id='ordermsg'] > textarea", "This is a test order");
+    actionOnPage.clickButton("a[href='/payment']");
+    utils.isPageUrlCorrect("/payment");
+    paymentPage.typePaymentDetails(this.paymentData);
+    actionOnPage.clickButton("button[id='submit']");
+    utils.isStringContains("div.col-sm-9.col-sm-offset-1 > p", "Congratulations! Your order has been confirmed!");
+    //cy.deleteUser();
   });
 });
