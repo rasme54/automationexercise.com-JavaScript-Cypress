0ef071488d43b95ae36a6557a768dd636af4a7f8
rasme54 <rsmektala54@gmail.com>
2025-03-03 00:24:09 +0100

    fixing cypress dashboard widget problems

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,19 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     projectId: "ttoywa",
-    baseUrl: 'https://automationexercise.com',
+    baseUrl: "https://automationexercise.com",
     includeShadowDom: true,
     chromeWebSecurity: true,
     viewportHeight: 1080,
     viewportWidth: 1920,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "reporter-config.json",
     },
   },
 });
